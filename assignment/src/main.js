6ef62d02fd1eb70754b6dd90c7ef10eaf1579c95
khanhnq <nqk1392002hg@gmail.com>
2022-01-14 22:36:29 +0700

    update all

--- assignment/src/main.js
+++ assignment/src/main.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/home";
 import NotFoundPage from "./pages/notFound";
 import ProductPage from "./pages/product";
 import NewsDetailPage from "./pages/newsDetail";
+import SingIn from "./pages/singin";
 import Dashboard from "./pages/admin/dashboard";
 import AdminNewsPage from "./pages/admin/news/listNews";
 import AdminAddNewsPage from "./pages/admin/news/addNews";
@@ -39,7 +40,11 @@ router.on({
     },
     "/admin/news/addNews": () => {
         print(AdminAddNewsPage.render());
+    },
+    "/singin": () => {
+        print(SingIn.render());
     }
+
 });
 router.notFound(() => print(NotFoundPage.render()));
 router.resolve();
